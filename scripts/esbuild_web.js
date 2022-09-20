65ce61e0d6088c108baf52feb5eefafe0d5cc549
Leif Riksheim <riksheim.leif@gmail.com>
2022-09-20 17:34:29 +0200

    sourcemap for development

--- scripts/esbuild_web.js
+++ scripts/esbuild_web.js
@@ -1,16 +1,16 @@
 const { default: litPlugin } = require("esbuild-plugin-lit");
 
+console.log(process.env.NODE_ENV);
+
 require("esbuild")
   .build({
     entryPoints: ["./src/web.ts"],
     bundle: true,
     format: "esm",
     minify: true,
-    sourcemap: false,
+    sourcemap: process.env.NODE_ENV === "dev" ? true : false,
     outfile: "dist/web.js",
     watch: process.env.NODE_ENV === "dev" ? true : false,
-    plugins: [
-      litPlugin()
-    ],
+    plugins: [litPlugin()],
   })
   .catch(() => process.exit(1));
