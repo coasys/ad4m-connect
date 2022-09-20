b793df20a9cc4c1f8d806f2f370ca25626d32265
Leif Riksheim <riksheim.leif@gmail.com>
2022-09-21 00:04:15 +0200

    chore: just throw error

--- src/utils.ts
+++ src/utils.ts
@@ -8,37 +8,15 @@ export async function checkPort(port: number) {
   try {
     const res = await fetch(`http://localhost:${port}/graphql`, {
       signal: Timeout().signal,
-      mode: 'no-cors'
+      mode: "no-cors",
     });
 
-    if (res.status == 0) {
-      return port
+    if (res.status === 0) {
+      return port;
     } else {
-      return null
+      return null;
     }
   } catch (e) {
-    console.error("failed", e);
-
-    if (Array.isArray(e)) {
-      if (
-        e.length > 0 &&
-        e[0].message.contains(
-          "Capability is not matched, you have capabilities:"
-        )
-      ) {
-        throw e[0];
-      }
-    }
-
-    if (
-      e.message ===
-      "Cannot extractByTags from a ciphered wallet. You must unlock first."
-    ) {
-      throw e;
-    }
-
-    if (e.message === "signature verification failed") {
-      throw e;
-    }
+    throw e;
   }
 }
