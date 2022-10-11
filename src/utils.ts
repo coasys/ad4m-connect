e9d439d4d0703e897b67028d3e904a262f1313fd
Fayeed <fayeed@live.com>
2022-10-11 19:59:50 +0530

    Moved utils to a seperate module

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { Ad4mClient } from "@perspect3vism/ad4m";
+
 function Timeout() {
   const controller = new AbortController();
   setTimeout(() => controller.abort(), 20);
@@ -20,3 +22,43 @@ export async function checkPort(port: number) {
     throw e;
   }
 }
+
+export function getAd4mClient(): Promise<Ad4mClient> {
+  return new Promise((resolve, reject) => {
+    document.addEventListener(
+      "return-fetch-ad4m-client",
+      function listener(event) {
+        this.removeEventListener("return-fetch-ad4m-client", listener);
+        // @ts-ignore
+        resolve(event.detail.ad4mClient);
+      }
+    );
+
+    const event = new CustomEvent("fetch-ad4m-client");
+    document.dispatchEvent(event);
+
+    setTimeout(() => {
+      reject("No Ad4mClient found");
+    }, 5000);
+  });
+}
+
+export function isConnected() {
+  return new Promise((resolve, reject) => {
+    document.addEventListener(
+      "return-fetch-ad4m-client",
+      function listener(event) {
+        // @ts-ignore
+        event.detail.addEventListener("connected_with_capabilities", () => {
+          this.removeEventListener("return-fetch-ad4m-client", listener);
+
+          resolve(true);
+        });
+      }
+    );
+
+    const event = new CustomEvent("fetch-ad4m-client");
+    document.dispatchEvent(event);
+  });
+}
+
